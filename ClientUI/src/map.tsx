32fb59cfea53b0c57a7cb97d0edd88628ea68842
Jéssica Olivença <47829707+JessicaOli@users.noreply.github.com>
2021-08-12 18:26:51 +0100

    Firestore listener implemented + marker stylization

--- ClientUI/src/map.tsx
+++ ClientUI/src/map.tsx
@@ -1,29 +1,57 @@
-import ReactMapboxGl,{Layer,Source,Feature} from 'react-mapbox-gl'
-import React,{useState,useRef,useEffect} from 'react'
-import {Marker} from './marker'
+import ReactMapboxGl from 'react-mapbox-gl'
+import {useState,useEffect} from 'react'
+import {Markers} from './marker'
+import db from './firebase.config'
 
 export function Map({token}:{token:string}){
     const [center,setCenter] = useState<Coordinates>({lat:-9.114954,long:38.756651})
     const [zoom, setZoom] = useState(17)
+    const [isSearching,setIsSearching] = useState(true)
 
     const Map = ReactMapboxGl({
       accessToken:
         'pk.eyJ1IjoiYTQ0ODE2IiwiYSI6ImNrcmF2YnRmMzFsOHoycGxwb3h1bGswc2sifQ.qJqfMCqIvQ_5ctcCo2v4qg'
     }); 
+
+    useEffect(()=>{
+      getDeliveryInfo()
+      async function getDeliveryInfo(){
+        const pendingDelivery=db.collection('PENDING_DELIVERIES')
+        
+        pendingDelivery.doc(token).get()
+        .then((doc)=>{
+          if(doc.exists){
+            setIsSearching(true)
+          }
+        })
+
+        pendingDelivery.onSnapshot((docSnapShot:any)=>{
+          docSnapShot.docChanges().forEach((change:any)=>{
+            if(change.type==='removed'&& change.doc.id===token){
+              setIsSearching(false)
+            }
+          })
+        }, err => {
+          console.log(`Encountered error: ${err}`);
+        })
+      }
+    },[])
     
 return(
-   <div>
-    <Map
-      style="mapbox://styles/mapbox/streets-v11"
-      containerStyle={{
-        height: '90vh',
-        width: '100vw'
-      }}
-      center={[center.lat,center.long]}
-      zoom={[zoom]}
-    >
-      <Marker token={token}/>
-    </Map>
+   <div>  
+      <div>
+        <Map
+          style="mapbox://styles/mapbox/streets-v11"
+          containerStyle={{
+            height: '90vh',
+            width: '100vw'
+          }}
+          center={[center.lat,center.long]}
+          zoom={[zoom]}
+        >
+        <Markers token={token}/>
+        </Map>
+      </div>
     </div>
 )
 }
--- ClientUI/src/marker.tsx
+++ ClientUI/src/marker.tsx
@@ -1,52 +1,84 @@
 import ReactMapboxGl,{Layer,Feature} from 'react-mapbox-gl'
 import React,{useState,useEffect} from 'react'
 import db from './firebase.config'
-const markerUrl = 'https://lh3.googleusercontent.com/proxy/02fLLeFaaVq-tKSbqajs3nsVTsuENFgGpxafRXm8g8fyzFIXaDXv5qdgHb1igdop7z1oiTEHBXoVUYJ75-o7ndBrs1fb8u3_axJ_ZNXvhuy_QavZlOsBUg'
 
-export function Marker({token}:{token:string}){
+export function Markers({token}:{token:string}){
     const [warperCoord,setWarperCoord] = useState<Coordinates>({lat:null,long: null})
     const [clientCoord,setClientCoord] = useState<Coordinates>({lat:null,long:null})
     const [storeCoord,setStoreCoord] = useState<Coordinates>({lat:null,long:null})
-    //Marker animation test
-    /*useEffect(() => {
-      const interval = setInterval(() => {
-        let newCoord = coordinates[count]
-        setWarperCoord({lat:newCoord.lat,long:newCoord.long})
-        setCount(count=>count+1)
-        console.log(count)
-      }, 3000);
-      return () => clearInterval(interval);
-    }, [count]);*/
 
     useEffect(() => {
       getDeliveryInfo()
 
       async function getDeliveryInfo(){
         const response=db.collection('DELIVERINGWARPERS')
-        const data = await response.where('token',"==",token).get()
+        const query = response.where('token',"==",token)
+
+        const data = await query.get()
         if (data.empty) {
           console.log('Sorry, we could not find a delivery matching that token.');
           return;
-        }  
+        } 
         data.forEach(doc => {
-          setWarperCoord({lat:doc.data().location.latitude,long:doc.data().location.longitude})
           setClientCoord({lat:doc.data().delivery.deliveryLocation.latitude,long:doc.data().delivery.deliveryLocation.longitude})
           setStoreCoord({lat:doc.data().delivery.pickUpLocation.latitude,long:doc.data().delivery.pickUpLocation.longitude})
-        });
+        }); 
+
+        const listener = await query.onSnapshot((docSnapShot:any)=>{
+          docSnapShot.docChanges().forEach((change:any)=>{
+            setWarperCoord({lat:change.doc.data().location.latitude,long:change.doc.data().location.longitude})
+          })
+        }, err => {
+          console.log(`Encountered error: ${err}`);
+        })
       }
     }, [])
        
 return(
    <div>
-    <Layer type="circle" id="marker" paint={{
-        'circle-color': "#ff5200",
-        'circle-stroke-width': 10,
-        'circle-stroke-color': '#ff5200',
-        'circle-stroke-opacity': 1
-      }}>
+
+      <Layer type="circle" id="marker" paint={{
+                'circle-color': "#ff5200",
+                'circle-stroke-width': 10,
+                'circle-stroke-color': '#ff5200',
+                'circle-stroke-opacity': 1
+              }}>
         <Feature coordinates={[warperCoord.long,warperCoord.lat]}/>
-        <Feature coordinates={[clientCoord.long, clientCoord.lat]}/>
-        <Feature coordinates={[storeCoord.long, storeCoord.lat]}/>
+        <Feature coordinates={[clientCoord.long,clientCoord.lat]}/>
+        <Feature coordinates={[storeCoord.long,storeCoord.lat]}/>
+      </Layer>
+
+      <Layer type="symbol" layout={{
+                'text-field': 'Warper',
+                'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+                'text-size': 11,
+                'text-transform': 'uppercase',
+                'text-letter-spacing': 0.05,
+                'text-offset': [0, 1.5]
+              }}>
+        <Feature coordinates={[warperCoord.long,warperCoord.lat]}/>
+      </Layer>
+
+      <Layer type="symbol" layout={{
+                'text-field': 'Loja',
+                'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+                'text-size': 11,
+                'text-transform': 'uppercase',
+                'text-letter-spacing': 0.05,
+                'text-offset': [0, 1.5]
+              }}>
+        <Feature coordinates={[storeCoord.long,storeCoord.lat]}/>
+      </Layer>
+
+      <Layer type="symbol" layout={{
+                'text-field': 'Cliente',
+                'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+                'text-size': 11,
+                'text-transform': 'uppercase',
+                'text-letter-spacing': 0.05,
+                'text-offset': [0, 1.5]
+              }}>
+        <Feature coordinates={[clientCoord.long,clientCoord.lat]}/>
       </Layer>
     </div>
 )
